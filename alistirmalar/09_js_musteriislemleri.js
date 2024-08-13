f89798df0256474e556824639a94b3244d7b45d9
bilgejava6 <bilgejava6@gmail.com>
2024-08-13 19:34:58 +0300

    ...

--- alistirmalar/09_js_musteriislemleri.js
+++ alistirmalar/09_js_musteriislemleri.js
@@ -1,4 +1,5 @@
 let musteriListesi = [];
+let editIndex = -1;
 function kaydet() {
     /**
      * Öncelikle tüm bileşenlerin bilgilerini çek
@@ -8,16 +9,71 @@ function kaydet() {
     let ad = document.getElementById('ad').value;
     let soyad = document.getElementById('soyad').value;
     let adres = document.getElementById('adres').value;
-    musteriListesi.push({ ad, soyad, adres });
-    tabloyuDoldur();
+   if(editIndex>-1){// edit işlemi
+        musteriListesi[editIndex].ad = ad;
+        musteriListesi[editIndex].soyad = soyad;
+        musteriListesi[editIndex].adres = adres;  
+        tabloyuDoldur();  
+        temizle();    
+   }else{ // yeni kayıt işlemi
+       
+        if(ad.trim()==='' || soyad.trim()==='' || adres.trim()==='')
+            swal({
+                title: "HATA!",
+                text: "Lütfen tüm alanları doldurun",
+                icon: "error",
+                button: "Tamam",
+            });
+        else{
+            musteriListesi.push({ ad, soyad, adres });
+            tabloyuDoldur();
+            swal({
+                title: "Kayıt işlemi",
+                text: "Müşteri baraşı ile kayıt edilmiştir.",
+                icon: "success",
+                button: "Tamam",
+            });
+            temizle();
+        }
+   }
+    
+   editIndex = -1;
 }
-function duzenle() { }
+function duzenle(index) {
+    let musteri = musteriListesi[index];
+    document.getElementById('ad').value=musteri.ad;
+    document.getElementById('soyad').value=musteri.soyad;
+    document.getElementById('adres').value=musteri.adres;
+    editIndex = index;
+ }
 
 function sil(index) { 
-    musteriListesi.splice(index,1);
-    tabloyuDoldur();
+    swal({
+        title: "DİKKAT!!!",
+        text: "Seçtiğiniz Kullanıcıyı silmek istiyor musunuz?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          musteriListesi.splice(index,1);
+          tabloyuDoldur();
+          swal("Kayıt başarı ile silindi", {
+            icon: "success",
+          });
+        } else {
+          swal("Silme işlemi iptal edildi.");
+        }
+      });
+      
+   
 }
-function temizle() { }
+function temizle() {
+    document.getElementById('ad').value='';
+    document.getElementById('soyad').value='';
+    document.getElementById('adres').value='';    
+ }
 
 function tabloyuDoldur() {
     let tablo = document.getElementById('musteriListesi');
@@ -32,7 +88,7 @@ function tabloyuDoldur() {
                 <td><img src="https://picsum.photos/50/50" alt="" id="p_image"></td>
                 <td>
                     <button onclick="sil(${index})" type="button" class="btn btn-danger"> X </button>
-                    <button onclick="duzenle()" type="button" class="btn btn-info"> # </button>
+                    <button onclick="duzenle(${index})" type="button" class="btn btn-info"> # </button>
                                 
                 </td>
             </tr>        
