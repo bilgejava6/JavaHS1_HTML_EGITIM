4f839201565760cef24ed2c08c37ac1b17346204
bilgejava6 <bilgejava6@gmail.com>
2024-08-18 15:10:34 +0300

    proje son hali

--- ReactKodlamalarim/ornek1/src/components/molecules/ProductCard.tsx
+++ ReactKodlamalarim/ornek1/src/components/molecules/ProductCard.tsx
@@ -1,6 +1,6 @@
 import { Rating } from '@mui/material'
 import React from 'react'
-
+import './ProductCard.css'
 interface IProductProps{
     urunAdi: string,
     urunFiyati: number,
@@ -11,7 +11,7 @@ interface IProductProps{
 function ProductCard(urun: IProductProps) {
   return (
     <div className="col-3 p-4">
-        <div className="row border rounded-4 p-2">
+        <div className="row border rounded-4 p-2 product">
             <div className="col">
                 <div className="row">
                     <img className='mb-1' src={urun.urunResim} alt="" />
@@ -22,11 +22,11 @@ function ProductCard(urun: IProductProps) {
                 <div className="row">
                     <label className='form-label mb-1'>{urun.urunFiyati} $</label>
                 </div>
-                <div className="row">
+                <div className="row mb-5">
                     <Rating readOnly value={urun.rate} />
                 </div>
-                <div className="row">
-
+                <div className="row mb-1 ps-4 pe-4">
+                    <button className='btn btn-info'>Sepete Ekle</button>
                 </div>
                 
 
